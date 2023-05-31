16d9042fb6f22c600027b97ba0139ab506816b94
Giovanne Rolo <giovannerolo@outlook.com>
2023-05-31 01:59:25 -0300

    Library created

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -44,7 +45,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
         "
       >
         <Box>
-          <div className="flex flex-col gap-y-4 py-4">
+          <div className="flex flex-col gap-y-4 px-5 py-4">
             {routes.map((item) => (
               <SidebarItem 
                 key={item.label}
@@ -54,9 +55,12 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
           </div>
         </Box>
         <Box className="overflow-y-auto h-full">
-          Song Library
+          <Library />
         </Box>
       </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">
+        {children}
+      </main>
     </div>
   )
 }
